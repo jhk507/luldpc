4bc66a4809f690cdfca070460e7b47502c70d466
gtoombs <gtoombs@lakeheadu.ca>
2009-03-23 04:38:02 +0000

    Comment updates

--- src/ldpc.hpp
+++ src/ldpc.hpp
@@ -27,8 +27,10 @@ namespace LDPC
 	// Returns true if no error
 	bool decode();
 
+	// Update the R matrix (belief propagation method)
 	void rupdate_bp();
 
+	// Update the R matrix (offset minsum method)
 	void rupdate_offms();
 
 	// Set the parity matrix based on the message
