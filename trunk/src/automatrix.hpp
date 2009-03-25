36eba5a6711ac74e034e9b89d4ff46d3544ab0e8
gtoombs@lakeheadu.ca <gtoombs@lakeheadu.ca@dadb8772-1654-11de-a6cc-771c510851eb>
2009-03-25 08:37:43 +0000

    Added all sorts of comments, and fixed a major bug in PreachingBased

--- trunk/src/automatrix.hpp
+++ trunk/src/automatrix.hpp
@@ -8,7 +8,7 @@
 
 #include <ostream>
 
-
+// Output a large column or row matrix of length Z*X; values are tab-separated
 template <typename Elm, int X, int Z>
 void outputLarge(const Elm *edata, std::ostream &out)
 {
@@ -22,6 +22,8 @@ void outputLarge(const Elm *edata, std::ostream &out)
 	out << std::endl;
 }
 
+// Output a large column or row matrix of length Z*X; values are not separated;
+// Z blocks are separated.
 template <typename Elm, int X, int Z>
 void outputLargeContiguous(const Elm *edata, std::ostream &out)
 {
@@ -35,61 +37,64 @@ void outputLargeContiguous(const Elm *edata, std::ostream &out)
 	out << std::endl;
 }
 
-
+// Class to store an array of arbitrary type
 template <typename Elm, typename Array>
 class Automatrix
 {
-protected:
-	Automatrix()
-	{
-	}
-
 public:
+	// Get a reference to the array
 	inline operator Array&()
 	{
 		return (Array&)*data;
 	}
 
+	// Get a pointer to the array data, offset by off
 	inline Elm *getData(int off)
 	{
 		return data+off;
 	}
 
+	// Output the array, separated by tabs
 	template <int X, int Z>
-	void outputLarge(std::ostream &out) const
+	inline void outputLarge(std::ostream &out) const
 	{
 		::outputLarge<Elm,X,Z>(data, out);
 	}
 
+	// Output the array; only Z blocks are separated by tabs
 	template <int X, int Z>
-	void outputLargeContiguous(std::ostream &out) const
+	inline void outputLargeContiguous(std::ostream &out) const
 	{
 		::outputLargeContiguous<Elm,X,Z>(data, out);
 	}
 
-
-
-protected:
+private:
+	// The array data
 	Array data;
 };
 
+// A single-dimension (row or column) basic matrix
 template <typename Elm, int X>
 class Automatrix1 : public Automatrix<Elm, Elm[X]>
 {
 };
 
+// A dual-dimension basic matrix
 template <typename Elm, int Y, int X>
 class Automatrix2 : public Automatrix<Elm, Elm[Y][X]>
 {
 };
 
+// An 'alias' into a part of a dual-dimension matrix.
 template <typename Elm, int Y, int X>
 class Automatrix2Alias
 {
 public:
+	// The type representing a row array.
 	typedef const Elm (&SubArrayRef)[X];
 
 public:
+	// The constructor. Set the data pointers based on the provided offset.
 	template <int XH>
 	Automatrix2Alias(const Elm (&original)[Y][XH], int off)
 	{
@@ -97,12 +102,14 @@ public:
 			data[y] = &original[y][off];
 	}
 
-
+	// Make the first [] operator return a reference to a row, suitable for
+	// a second [].
 	inline SubArrayRef operator[](int y) const
 	{
 		return (SubArrayRef)*data[y];
 	}
 
 private:
+	// The array of shifted row pointers.
 	const Elm *data[Y];
 };
--- trunk/src/preachingbased.hpp
+++ trunk/src/preachingbased.hpp
@@ -12,46 +12,69 @@
 
 //////////////////////////////////////////////////////////////////////////////
 
+// A matrix with the same dimensions as the Preaching matrix, and with values
+// in the same places of the stored sparse matrix as in those of the Preaching
+// matrix.
 template <typename Elm, int Y, int X, int YRHO, int XRHO>
 class PreachingBased
 {
 public:
+	// An element structure containing the value at certain coordinates
+	// as well as a pointer to the next value in the y direction.
 	struct LinkedElm
 	{
 		Elm val;
 		LinkedElm *nexty;
-	//	inline operator Elm&() { return val; }
 	};
 
 public:
+	// The constructor. Takes a reference to an existing Preaching object.
 	PreachingBased(const Preaching<Y,X,YRHO,XRHO> &preachingInit);
 
+	// The destructor.
 	~PreachingBased();
 
+	// Output the entire matrix.
 	void output(std::ostream &out) const;
 
+	// Iterate through a column.
 	template <typename Functor>
 	inline void iterX(int x) const;
 
+	// Iterate through a row.
 	template <typename Functor>
 	inline void iterY(int y) const;
 
+	// Iterate through a column of this matrix and another PreachingBased
+	// matrix at the same coordinates.
 	template <typename Functor>
 	inline void iterX2(int x, PreachingBased &p2) const;
 
+	// Iterate through a row of this matrix and another PreachingBased
+	// matrix at the same coordinates.
 	template <typename Functor>
 	inline void iterY2(int y, PreachingBased &p2) const;
 
+	// Iterate through a column of this matrix and two other PreachingBased
+	// matrices at the same coordinates.
 	template <typename Functor>
 	inline void iterX3(int x, PreachingBased &p2, PreachingBased &p3) const;
 
+	// Iterate through a row of this matrix and two other PreachingBased
+	// matrices at the same coordinates.
 	template <typename Functor>
 	inline void iterY3(int y, PreachingBased &p2, PreachingBased &p3) const;
 
 public:
+	// The main data array. This is stored in order of occurrence from left to
+	// right, then top to bottom.
 	LinkedElm *data;
+	// The row pointers. The very last pointer is invalid and is used to denote
+	// the end of the array for iterators.
 	LinkedElm *Vyc[Z*Y+1];
+	// The column pointers.
 	LinkedElm *Vxc[Z*X];
+	// A reference to the Preaching matrix.
 	const Preaching<Y,X,YRHO,XRHO> &preaching;
 };
 
@@ -60,46 +83,63 @@ template <typename Elm, int Y, int X, int YRHO, int XRHO>
 PreachingBased<Elm,Y,X,YRHO,XRHO>::PreachingBased(const Preaching<Y,X,YRHO,XRHO> &preachingInit) :
 	preaching(preachingInit)
 {
+	// Allocate the memory for the main data array.
 	data = new LinkedElm[preaching.ones];
 
+	// Store static pointers to the Vyc and Vxc members for use in the functor.
 	static LinkedElm **rVyc, **rVxc;
 	rVyc = Vyc;
 	rVxc = Vxc;
+	// Store a static pointer to the Preaching instance's Hyc array for use in
+	// the functor.
 	static const int (*rHyc)[Z*Y][YRHO+1];
 	rHyc = &preaching.Hyc;
 
+	// A pointer to the current data element.
 	LinkedElm *dati = data;
-	for (int y = 0;; y++)
+	for (int y = 0;; y++)	// Iterate over the rows.
 	{
-		rVyc[y] = dati;
+		rVyc[y] = dati;		// Store the start of the row.
 		if (y >= Z*Y)
 			break;
+		// A pointer to the compressed row in the Preaching matrix.
 		const int *pHyc = (*rHyc)[y];
-		while (*pHyc++ >= 0)
+		while (*pHyc++ >= 0)	// Find the end of the row.
 			dati++;
+		dati++;				// Skip the terminator.
 	}
 
-	for (int x = 0; x < Z*X; x++)
+	for (int x = 0; x < Z*X; x++)	// Iterate over the columns.
 	{
+		// Remember the element previous to this one in the column.
 		static LinkedElm *prev;
 		prev = 0;
+
+		// The functor called for every element in the column.
 		struct functor_setlinks {
 			static inline void callbackX(int y, int x) {
+				// A pointer to the current element in the row.
 				LinkedElm *currentelm = rVyc[y];
+				// A pointer to the current element in the compressed Preaching
+				// matrix row.
 				const int *currenth = (*rHyc)[y];
+				// Find the element at (y,x).
 				while (*currenth != x)
 				{
+					// Increment both the Preaching and PreachingBased pointers.
 					currenth++;
 					currentelm++;
 				}
-				if (prev)
-					prev->nexty = currentelm;
-				else
-					rVxc[x] = currentelm;
-				prev = currentelm;
+				if (prev)						// We're not at the beginning of the column.
+					prev->nexty = currentelm;	// Set the previous link.
+				else							// We're at the beginning of the column.
+					rVxc[x] = currentelm;		// Set the column's beginning.
+				prev = currentelm;				// Update the prev pointer.
 			}
 		};
+		// Iterate through every element in the column.
 		preaching.iterX<functor_setlinks>(x);
+		// Terminate the column.
 		prev->nexty = 0;
 	}
 }
@@ -107,6 +147,7 @@ PreachingBased<Elm,Y,X,YRHO,XRHO>::PreachingBased(const Preaching<Y,X,YRHO,XRHO>
 template <typename Elm, int Y, int X, int YRHO, int XRHO>
 PreachingBased<Elm,Y,X,YRHO,XRHO>::~PreachingBased()
 {
+	// Free the main data array.
 	delete [] data;
 }
 
