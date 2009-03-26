4003f2361d2e37e2434181d9071dc32d74cfb227
gtoombs@lakeheadu.ca <gtoombs@lakeheadu.ca@dadb8772-1654-11de-a6cc-771c510851eb>
2009-03-26 06:15:51 +0000

    Made a "subfunction"

--- trunk/graph.m
+++ trunk/graph.m
@@ -2,50 +2,32 @@
 % $Date$
 % $Rev$
 
-clear;
+function graph
+    clear;
+    loadHist('hist_orth.tsv', 'Orthagonal', 1);
+    loadHist('hist_mess.tsv', 'Message', 2);
+end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Load the histogram data.
-load hist_orth.tsv;
+function loadHist(file, error, nplot)
+    % Load the histogram data.
+    hist = load(file);
 
-% Calculate the histogram dimensions.
-niters   = size(hist_orth,1)-1;
-nbuckets = size(hist_orth,2)-1;
+    % Calculate the histogram dimensions.
+    niters   = size(hist,1)-1;
+    nbuckets = size(hist,2)-1;
 
-% Extract the histogram matrices.
-iters   = hist_orth(2:(niters+1),1);
-buckets = hist_orth(1,2:(nbuckets+1));
-freqs   = hist_orth(2:(niters+1),2:(nbuckets+1));
+    % Extract the histogram matrices.
+    iters   = hist(2:(niters+1),1);
+    buckets = hist(1,2:(nbuckets+1));
+    freqs   = hist(2:(niters+1),2:(nbuckets+1));
 
-% Plot the surface.
-subplot(1,2,1)
-surf(buckets, iters, freqs)
+    % Plot the surface.
+    subplot(1,2,nplot)
+    surf(buckets, iters, freqs)
 
-% Make the labels.
-title('Orthagonal error histogram')
-xlabel('Orthagonal error')
-ylabel('Iteration number')
-zlabel('Frequency')
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Load the histogram data.
-load hist_mess.tsv;
-
-% Calculate the histogram dimensions.
-niters   = size(hist_mess,1)-1;
-nbuckets = size(hist_mess,2)-1;
-
-% Extract the histogram matrices.
-iters   = hist_mess(2:(niters+1),1);
-buckets = hist_mess(1,2:(nbuckets+1));
-freqs   = hist_mess(2:(niters+1),2:(nbuckets+1));
-
-% Plot the surface.
-subplot(1,2,2)
-surf(buckets, iters, freqs)
-
-% Make the labels.
-title('Message error histogram')
-xlabel('Message error')
-ylabel('Iteration number')
-zlabel('Frequency')
+    % Make the labels.
+    title([error, ' error histogram'])
+    xlabel([error, ' error'])
+    ylabel('Iteration number')
+    zlabel('Frequency')
+end
