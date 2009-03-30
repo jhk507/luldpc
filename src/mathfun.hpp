d20d657259438c71d1b0033b9c7a922e84c97dc1
gtoombs <gtoombs@lakeheadu.ca>
2009-03-30 19:36:48 +0000

    Fixed mathfun newlines

--- src/mathfun.hpp
+++ src/mathfun.hpp
@@ -1,7 +1,7 @@
 /*
-* $URL: https://luldpc.googlecode.com/svn/trunk/src/preachingbased.hpp $
-* $Date: 2009-03-25 21:01:53 -0400 (Wed, 25 Mar 2009) $
-* $Rev: 30 $
+* $URL$
+* $Date$
+* $Rev$
 */
 
 #pragma once
