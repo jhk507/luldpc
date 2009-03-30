e80f5bf5bde1269c8b90fe35385f4480f1ce1c21
gtoombs@lakeheadu.ca <gtoombs@lakeheadu.ca@dadb8772-1654-11de-a6cc-771c510851eb>
2009-03-30 01:31:29 +0000

    Axis and camera graph work

--- trunk/matlab/graph.m
+++ trunk/matlab/graph.m
@@ -13,30 +13,30 @@ function graph
 
     % Display the error/iteration/frequency surface
     figure(1);
-    graph_error('bp_orth',    'BP Orthagonal',    1, axis_err_orth);
-    graph_error('bp_mess',    'BP Message',       2, axis_err_mess);
-    graph_error('offms_orth', 'OFFMS Orthagonal', 3, axis_err_orth);
-    graph_error('offms_mess', 'OFFMS Message',    4, axis_err_mess);
+    graph_error('bp_orth',    'BP Orthagonal',    axis_err_orth, [-180 2    4.5], 1);
+    graph_error('bp_mess',    'BP Message',       axis_err_mess, [-180 0.35 4.5], 2);
+    graph_error('offms_orth', 'OFFMS Orthagonal', axis_err_orth, [-180 2    4.5], 3);
+    graph_error('offms_mess', 'OFFMS Message',    axis_err_mess, [-180 0.35 4.5], 4);
 
     % Display the snr/iteration/frequency surface
     figure(2);
-    graph_snr('bp_orth',    'BP Orthagonal',    1, axis_snr);
-    graph_snr('bp_mess',    'BP Message',       2, axis_snr);
-    graph_snr('offms_orth', 'OFFMS Orthagonal', 3, axis_snr);
-    graph_snr('offms_mess', 'OFFMS Message',    4, axis_snr);
+    graph_snr('bp_orth',    'BP Orthagonal',    axis_snr, 1);
+    graph_snr('bp_mess',    'BP Message',       axis_snr, 2);
+    graph_snr('offms_orth', 'OFFMS Orthagonal', axis_snr, 3);
+    graph_snr('offms_mess', 'OFFMS Message',    axis_snr, 4);
     
     % Display the error/snr/frequency surface
     figure(3);
-    graph_maxiter('bp_orth',    'BP Orthagonal',    1, axis_err_orth, axis_snr);
-    graph_maxiter('bp_mess',    'BP Message',       2, axis_err_mess, axis_snr);
-    graph_maxiter('offms_orth', 'OFFMS Orthagonal', 3, axis_err_orth, axis_snr);
-    graph_maxiter('offms_mess', 'OFFMS Message',    4, axis_err_mess, axis_snr);
+    graph_maxiter('bp_orth',    'BP Orthagonal',    axis_err_orth, axis_snr, [2    -4 4], 1);
+    graph_maxiter('bp_mess',    'BP Message',       axis_err_mess, axis_snr, [0.35 -4 4], 2);
+    graph_maxiter('offms_orth', 'OFFMS Orthagonal', axis_err_orth, axis_snr, [2    -4 4], 3);
+    graph_maxiter('offms_mess', 'OFFMS Message',    axis_err_mess, axis_snr, [0.35 -4 4], 4);
     
     % Display the snr/iteration/error/frequency volumetric slices
     figure(4);
-    graph_slice('bp_orth',    'BP Orthagonal',    1, axis_snr, axis_err_orth);
-    graph_slice('offms_orth', 'OFFMS Orthagonal', 2, axis_snr, axis_err_orth);
+    graph_slice('bp_orth',    'BP Orthagonal',    axis_snr, axis_err_orth, [-450 5 2.5], 1);
+    graph_slice('offms_orth', 'OFFMS Orthagonal', axis_snr, axis_err_orth, [-450 5 2.5], 2);
     figure(5);
-    graph_slice('bp_mess',    'BP Message',    1, axis_snr, axis_err_mess);
-    graph_slice('offms_mess', 'OFFMS Message', 2, axis_snr, axis_err_mess);
+    graph_slice('bp_mess',    'BP Message',    axis_snr, axis_err_mess, [-900 2.25 4.5], 1);
+    graph_slice('offms_mess', 'OFFMS Message', axis_snr, axis_err_mess, [-900 2.25 4.5], 2);
 end
