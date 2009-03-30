e079c3099b0240f3d5359ef847f45682a8132801
gtoombs <gtoombs@lakeheadu.ca>
2009-03-30 02:08:53 +0000

    Removed lines from SNR slices in slice graph

--- matlab/graph_slice.m
+++ matlab/graph_slice.m
@@ -26,15 +26,23 @@ function graph_slice(filename, titlename, axis_snr, axis_err, cam, nplot)
     [cx, cy, cz] = meshgrid(axis_iter, axis_err, axis_snr);
 
     % Do the slice plot.
-    subplot(1,2,nplot)
-    slice(cx, cy, cz, vol, niters-1, 0, axis_snr)
+    subplot(1,2,nplot);
+    hslice = slice(cx, cy, cz, vol, niters-1, 0, axis_snr);
 
     % Make the labels.
-    title([titlename, ' error histogram'])
-    xlabel('Iteration number')
-    ylabel([titlename, ' error'])
-    zlabel('SNR (dB)')
+    title([titlename, ' error histogram']);
+    xlabel('Iteration number');
+    ylabel([titlename, ' error']);
+    zlabel('SNR (dB)');
 
+    % Set the axis style.
     axis tight;
+    % Set the camera position.
     campos(cam);
+    % Remove lines on the SNR slices.
+    for hi = 1:length(hslice)
+        if (size(get(hslice(hi), 'XData')) == [nerrs niters])
+            set(hslice(hi), 'LineStyle', 'none');
+        end
+    end
 end
