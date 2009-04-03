29e7a5b11785ca53ea70e2747d8b40987976fc45
gtoombs <gtoombs@lakeheadu.ca>
2009-04-03 17:33:55 +0000

    More inlining

--- src/itime.cpp
+++ src/itime.cpp
@@ -10,17 +10,10 @@ namespace LDPC
 {
 
 #ifdef WIN32
+
 LARGE_INTEGER ITime::freq;
 bool ITime::freqInitialized = QueryPerformanceFrequency(&ITime::freq);
-#endif
 
-void ITime::reset()
-{
-#ifdef WIN32
-	QueryPerformanceCounter(&start);
-#else
-	gettimeofday(&start, 0);
 #endif
-}
 
 };
